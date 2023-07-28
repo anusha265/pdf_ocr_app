6002022fea473c735c719aab5cc9c1a3c117aac2
anusha bandaru <bandaru.anusha21@ifheindia.org>
2023-07-28 11:45:59 +0530

    first commit

---
+++ pdf_ocr_app/urls.py
@@ -0,0 +1,9 @@
+# pdf_ocr_app/urls.py
+
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', include('ocr_converter.urls')),
+]
